31be01a70b1ed3d1d96889575638b9e6c3e49897
Alberto H <foliolo7891@gmail.com>
2017-02-27 01:59:03 +0100

    Agregar archivos de proyecto.

---
+++ LoadSPD/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadSPD
+{
+    class Paciente
+    {
+        public String Name { get; set; }
+        public String Expiracy { get; set; }
+
+        public Paciente (String Name, String Expiracy)
+        {
+            this.Name = Name;
+            this.Expiracy = Expiracy;
+        }
+    }
+}
---
+++ LoadSPD/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using Firebase.Database;
+using System.Threading.Tasks;
+using Firebase.Database.Query;
+
+namespace LoadSPD
+{
+    class Program
+    {
+        private static List<Paciente> pacientes = new List<Paciente>();
+
+        static void Main(string[] args)
+        {
+            CargarPacientes();
+
+            SubidaFirebase().Wait();
+
+            Console.WriteLine("Pulsa para salir...");
+            Console.Read();
+        }
+
+     
+        /// <summary>
+        /// Lectura del fichero para cargar los pacientes en la lista.
+        /// </summary>
+        private static void CargarPacientes()
+        {
+            Application myApplication = new Application();
+            myApplication.Visible = true;
+            Workbook myWorkbook = myApplication.Workbooks.Open(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + @"renovacion_spd.xls");
+            Worksheet myWorksheet = myWorkbook.Sheets["Hoja1"] as Worksheet;
+
+
+            for (int row = 2; row < myWorksheet.Rows.Count; row++)
+            {
+                Range NameRange = (Range)myWorksheet.Cells[row, 1];
+                Range ExpiracyRange = (Range)myWorksheet.Cells[row, 3];
+
+                if (String.IsNullOrEmpty(NameRange.Text.ToString()))
+                    break;
+
+                pacientes.Add(new Paciente(NameRange.Text.ToString(), ExpiracyRange.Text.ToString()));
+
+            }
+        }
+
+        /// <summary>
+        /// Subida de la lista de pacientes a firebase
+        /// </summary>
+        private static async Task SubidaFirebase()
+        {
+            var client = new FirebaseClient("https://spds-22cc3.firebaseio.com/");
+
+            // delete entire conversation list
+            await client.Child("pacientes").DeleteAsync();
+            // load data
+            await client.Child("lista_pacientes").PutAsync(pacientes);
+
+            foreach (Paciente paciente in pacientes)
+            {
+                await client.Child("pacientes").Child(paciente.Name).PutAsync(paciente.Expiracy);
+            }
+
+            client.Child("pacientes").Dispose();
+        }
+    }
+}
